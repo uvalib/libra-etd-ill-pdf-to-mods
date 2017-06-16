0e12f24c110353a6fad23f895e04e63bb7fd6bc5
UVADave <dpg3k@virginia.edu>
2017-06-16 10:46:19 -0400

    Update workset creation to use mapping file

--- generateworkset/generateworkset.rb
+++ generateworkset/generateworkset.rb
@@ -2,85 +2,33 @@
   TEMPDIR='tmp'
 
   #
-  # get a list of assets in the specified directory that match the supplied pattern
+  # get a mapping of the work ids and supporting file(s)
   #
-  def get_file_list( dirname, pattern )
-    res = []
-    begin
-      Dir.foreach( dirname ) do |f|
-        if pattern.match( f )
-          res << f
-        end
+  def get_mapping_list( filename )
+    res = {}
+
+    File.open( filename ).each do |line|
+      tokens = line.split( "," )
+      next if tokens.length < 2
+
+      fname = tokens[ 0 ]
+      id = tokens[ 1 ]
+      if res.include?( id )
+        res[ id ] << fname unless res[ id ].include? fname
+      else
+        res[ id ] = [ fname ]
       end
-    rescue => e
     end
 
     return res
   end
 
   #
-  # extract the barcode from the specified filename
+  # create workset file using the provided id, metadata and asset file names
   #
-  def barcode_from_filename( filename )
-     return filename[ 0..9 ]
-  end
+  def create_workset( dirname, id, metadata_name, asset_names )
 
-  #
-  # extract the external ID from the specified barcode
-  #
-  def id_from_barcode( barcode )
-    f = File.join( TEMPDIR, "#{barcode}_index_out.txt" )
-    File.open( f ).each do |line|
-      if /^.* : id = /.match( line )
-        id = /^.* : id = (.*)$/.match( line ).captures[ 0 ]
-        return id
-      end
-    end
-    return nil
-  end
-
-  #
-  # return the asset filename given the metadata filename
-  #
-  def asset_filename_from_metadata_filename( filename )
-    return filename.gsub( '.xml', '' )
-  end
-
-  #
-  # return the metadata filename given the asset filename
-  #
-  def metadata_filename_from_asset_filename( filename )
-    return "#{filename}.xml"
-  end
-
-  #
-  # process each xml file and create a set of unique (external) ids
-  #
-  def build_id_set( files )
-
-    ids = {}
-
-    files.each do |f|
-      bc = barcode_from_filename( f )
-      id = id_from_barcode( bc )
-      next if id.nil?
-      if ids[ id ].nil?
-        ids[ id ] = [ asset_filename_from_metadata_filename( f ) ]
-      else
-        ids[ id ] << asset_filename_from_metadata_filename( f )
-      end
-    end
-
-    return ids
-  end
-
-  #
-  # create workset file using the provided metadata and asset file names
-  #
-  def create_workset( dirname, metadata_name, asset_names )
-
-    bc = barcode_from_filename( metadata_name )
-    f = File.join( dirname, "#{bc}.workset" )
+    f = File.join( dirname, "#{id}.workset" )
     File.open( f, 'w') do |file|
       file.write( "metadata : #{metadata_name}\n" )
       asset_names.each do |a|
@@ -93,27 +41,20 @@
   #
   # create the workset files
   #
-  def create_all_worksets( dirname, files, ids )
+  def create_all_worksets( dirname, maps )
 
-    files.each do|f|
-      bc = barcode_from_filename( f )
-      id = id_from_barcode( bc )
-      if id.nil?
-        puts "IGNORING #{f}!!!"
-        next
-      end
-      assets = ids[ id ]
+    maps.each do|id, assets|
 
-      if assets.length == 1
-        puts "Creating workset file for #{f} with 1 asset"
-        create_workset( dirname, f, assets )
-      else
-        if f == metadata_filename_from_asset_filename( assets.last )
-          puts "Creating workset file for #{f} with #{assets.length} assets"
-          create_workset( dirname, f, assets )
-        end
-      end
+      puts "Creating workset file for #{id} with #{assets.length} asset(s)"
+      assets = assets.map { |a| "#{a}.pdf" }
+      metadata_file = "#{assets[ 0 ]}.xml"
+      create_workset( dirname, id, metadata_file, assets )
     end
+  end
+
+  def usage( )
+    puts "use: ruby #{$PROGRAM_NAME} <mapping file> <work directory>"
+    exit( 0 )
   end
 
   #
@@ -122,28 +63,30 @@
   if __FILE__ == $PROGRAM_NAME
 
     # check arguments
-    working_dir = ARGV[ 0 ]
-    if working_dir.nil?
-      puts "use: ruby #{$PROGRAM_NAME} <work directory>"
-      exit( 0 )
-    end
+    map_file = ARGV[ 0 ]
+    usage( ) if map_file.nil?
 
-    if Dir.exists?( working_dir ) == false
-      puts "ERROR: directory #{working_dir} does not exist"
+    working_dir = ARGV[ 1 ]
+    usage( ) if working_dir.nil?
+
+    if File.exists?( map_file ) == false
+      puts "ERROR: file #{map_file} does not exist or is not readable, aborting"
       exit( 1 )
     end
 
-    # get a list of XML files
-    files = get_file_list( working_dir, /^.*\.xml$/ )
+    if Dir.exists?( working_dir ) == false
+      puts "ERROR: directory #{working_dir} does not exist or is not readable, aborting"
+      exit( 1 )
+    end
 
-    # build a list of id's to handle multi-volumes
-    ids = build_id_set( files )
-
-    #ids.each do |k, v|
+    # get a map of the ids and corresponding files
+    maps = get_mapping_list( map_file )
+    #maps.each do |k, v|
     #  puts "==> #{k} -> #{v}"
     #end
+
     # create the workset files
-    create_all_worksets( working_dir, files, ids )
+    create_all_worksets( working_dir, maps )
 
   end
 
